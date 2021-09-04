7cb019c81bbba96b9a6b3a689218fc94d67ef9a0
Chanuka Abeysinghe <abeysinghechanuka@gmail.com>
2021-09-04 16:29:09 +0530

    Nested components

---
+++ myreactapp/src/Ninjas.js
@@ -0,0 +1,14 @@
+import react , {Component} from "react";
+
+class Ninjas extends Component{
+    render(){
+        return(
+            <div className="ninja">
+                <div>Name : Chanuka Abeysinghe</div>
+                <div>Age : 23 </div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas;
